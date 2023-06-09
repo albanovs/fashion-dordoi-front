631f1c749026014263850cef0ae545a24bb3cb74
albvnovs <albvnovs@gmail.com>
2023-06-09 14:24:17 +0600

    login

--- src/page/login/login-page.jsx
+++ src/page/login/login-page.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
+import { Register } from './register-page';
 
 export default function LoginPage() {
     const [username, setUsername] = useState('');
@@ -16,7 +17,7 @@ export default function LoginPage() {
             const { token } = response.data;
 
             localStorage.setItem('token', token);
-            navigate('/settings');
+            navigate('/');
             window.location.reload()
             console.log('Вход выполнен успешно');
         } catch (error) {
@@ -28,6 +29,7 @@ export default function LoginPage() {
 
     return (
         <section className="bg-slate-900">
+            <Register/>
             <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto h-screen w-full lg:py-0">
                 <div className="w-full grid place-items-center">
                     <a href="/" className="text-white flex items-center mb-6 text-2xl font-semibold text-gray-900">
